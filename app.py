55ce147cfd48f2dc7561df10d9e1cac102ce51df
Vincent Yip <realsharkguy@gmail.com>
2024-08-11 23:52:44 -0400

    dependencies fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-import tool
+from tool import *
 from PIL import Image
 import requests
 from io import BytesIO
@@ -26,14 +26,14 @@ def main():
 
 
     #Display Ticker Symbol and Last Price
-    st.subheader(tool.get_company_name(ticker))
+    st.subheader(get_company_name(ticker))
 
     col1, col2, col3, col4 = st.columns(4)
-    low, high, yoy, last_year_price = tool.get_yoy_price_data(ticker)
+    low, high, yoy, last_year_price = get_yoy_price_data(ticker)
     with col1:
-        st.metric("Last Price", f"${tool.get_last_price(ticker):.2f}")
+        st.metric("Last Price", f"${get_last_price(ticker):.2f}")
     with col2:
-        st.metric("Price Difference (YoY)", f"${(tool.get_last_price(ticker)-last_year_price):.2f}", f"{yoy:+.2f}%")
+        st.metric("Price Difference (YoY)", f"${(get_last_price(ticker)-last_year_price):.2f}", f"{yoy:+.2f}%")
     with col3:
         st.metric("52-Week High", f"${low:.2f}")
     with col4:
@@ -44,8 +44,8 @@ def main():
     # candlestick_chart.update_layout(title=f"{symbol} Candlestick Chart", xaxis_rangeslider_visible=False)
     # st.plotly_chart(candlestick_chart, use_container_width=True)
     
-    callbelow_df, callabove_df = tool.get_relevant_call_options(ticker, range)
-    putbelow_df, putabove_df = tool.get_relevant_put_options(ticker, range)
+    callbelow_df, callabove_df = get_relevant_call_options(ticker, range)
+    putbelow_df, putabove_df = get_relevant_put_options(ticker, range)
     
     st.text(f"Call Options {range}% above the strike price")
     st.dataframe(callabove_df, height=int(35.2*(len(callabove_df)+1)))
